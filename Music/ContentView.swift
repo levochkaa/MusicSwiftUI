58c83d498190dc209a28a13721f07bc7d4521d99
Lev Poznyakov <levfirstgamer@mail.ru>
2022-03-02 19:13:45 +0300

    Initial Commit

---
+++ Music/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music
+//
+//  Created by Лев on 02.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Лев on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
